bfa7fcad2cf9911cb3e68c865546206989bdac1a
Sean Olson <SloughHand@gmail.com>
2020-12-12 19:36:41 -0800

    implemented fetchFiles()

--- test/files.test.js
+++ test/files.test.js
@@ -2,20 +2,18 @@ const fs = require('fs');
 const path = require('path');
 const files = require('../lib/fs/files');
 
-const filePath = (process.cwd() + path.sep + 'data-files' + path.sep).split(path.sep).join(path.posix.sep);
-
-test('files have been created', async () => {
+test('data files have been created', async () => {
+  const filePath = (process.cwd() + path.sep + 'data-files' + path.sep).split(path.sep).join(path.posix.sep);
   await files.generateFiles();
   const generatedFiles = fs.readdirSync(filePath);
   expect(generatedFiles.length).toBe(3);
 });
 
-
-test('files have been retrieved', async () => {
-  const files = await files.fetchFiles();
-  const fileCount = files.length;
+test('data-file contents have been retrieved', async () => {
+  const fetchedFiles = await files.fetchFiles();
+  const fileCount = fetchedFiles.length;
   let haveStrings = true;
-  files.forEach((file) => {
+  fetchedFiles.forEach((file) => {
     if(typeof file !== 'string'){
       haveStrings = false;
     }
