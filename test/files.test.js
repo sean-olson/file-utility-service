1b11e54a9f6daf984154acaf009efbe43d290b96
Sean Olson <SloughHand@gmail.com>
2020-12-12 18:56:47 -0800

    added test for fetchFiles()

--- test/files.test.js
+++ test/files.test.js
@@ -2,9 +2,25 @@ const fs = require('fs');
 const path = require('path');
 const files = require('../lib/fs/files');
 
+const filePath = (process.cwd() + path.sep + 'data-files' + path.sep).split(path.sep).join(path.posix.sep);
+
 test('files have been created', async () => {
-  const filePath = (process.cwd() + path.sep + 'data-files' + path.sep).split(path.sep).join(path.posix.sep);
   await files.generateFiles();
   const generatedFiles = fs.readdirSync(filePath);
   expect(generatedFiles.length).toBe(3);
 });
+
+
+test('files have been retrieved', async () => {
+  const files = await files.fetchFiles();
+  const fileCount = files.length;
+  let haveStrings = true;
+  files.forEach((file) => {
+    if(typeof file !== 'string'){
+      haveStrings = false;
+    }
+  });
+  expect(fileCount === 3 && haveStrings).toBe(true);
+});
+
+test.todo('parse files');
